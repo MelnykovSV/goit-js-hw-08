b99d33803e0a9cfd593cc840faac98e4e9f15e9a
MelnykovSV <svmelnykov@gmail.com>
2023-01-26 02:47:37 +0200

    submit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,55 @@
+const throttle = require('lodash.throttle');
+
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', e => {
-  console.log(e.target.value);
+const button = document.querySelector('.submit');
+
+// load from storage
+window.addEventListener('load', e => {
+  form.querySelector('input').value = localStorage.getItem('email');
+  form.querySelector('textarea').value = localStorage.getItem('message');
 });
+
+// write to storage
+form.addEventListener(
+  'input',
+  throttle(e => {
+    console.log(e.target.name);
+    localStorage.setItem(e.target.name, e.target.value);
+    console.log(`${e.target.name} value: ${e.target.value}`);
+
+    // localStorage.setItem(item[0], item[1]);
+    // const formData = new FormData(e.currentTarget);
+    // for (item of [...formData]) {
+    //   localStorage.setItem(item[0], item[1]);
+    // }
+
+    // console.log(`Email value: ${localStorage.getItem('email')}`);
+    // console.log(`Message value: ${localStorage.getItem('message')}`);
+  }, 2000)
+);
+
+// submit and clear storage
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+  for (item of [...formData]) {
+    localStorage.removeItem(item[0], item[1]);
+    console.log(`${item[0]} value: ${item[1]}`);
+  }
+
+  form.reset();
+});
+
+// form.addEventListener(
+//     'input',
+//     throttle(e => {
+//       const formData = new FormData(e.currentTarget);
+//       for (item of [...formData]) {
+//         localStorage.setItem(item[0], item[1]);
+//       }
+
+//       console.log(`Email value: ${localStorage.getItem('email')}`);
+//       console.log(`Message value: ${localStorage.getItem('message')}`);
+//     }, 500)
+//   );
