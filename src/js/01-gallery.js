6cdf300a959fbb7754b19657f66e04972474fbd6
MelnykovSV <svmelnykov@gmail.com>
2023-02-04 19:19:57 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
 //  ==========================MARKUP CREACTION==============================
 const markup = galleryItems
-  .map((ele, i, arr) => {
+  .map(ele => {
     return `<a class="gallery__item" href = "${ele.original}" data-capture="${ele.description}"><img class="gallery__image" src="${ele.preview}" alt="${ele.description}"></a>`;
   })
   .join('');
