0d6f90f3c31d44c71a95fd967df18af85f5095c1
MelnykovSV <svmelnykov@gmail.com>
2023-02-04 19:01:31 +0200

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,20 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+//  ==========================MARKUP CREACTION==============================
+const markup = galleryItems
+  .map((ele, i, arr) => {
+    return `<a class="gallery__item" href = "${ele.original}" data-capture="${ele.description}"><img class="gallery__image" src="${ele.preview}" alt="${ele.description}"></a>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+// //  ==========================EVENTS HANDLING==============================
+
+var $gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
