db834175ae0c30d4168930b995a419ad2db74a3e
MelnykovSV <svmelnykov@gmail.com>
2023-02-07 02:02:40 +0200

    form rework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,22 @@
 const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 
-// checking items in local storage and loading values
-window.addEventListener('load', () => {
-  // ???А навіщо тут проводити перевірку, чи є такий елемент в сховищі? Коли перевірки не було, воно просто підтягувало ті значення що були, а ті, яких не було, не підтягувало.
-  // При цьому навіть помилок в консоль не видавало.
+let formObject = {};
 
+// loading object from the local storage if one exists
+if (localStorage.getItem('feedback-form-state')) {
+  formObject = JSON.parse(localStorage.getItem('feedback-form-state'));
+}
+
+// checking items in object and loading values
+
+// ???? Чи можна прибрати цей EventListener? Тоді formData стане глобальною. Тут це ні на що не впливає, але, мені здається, зайві глобальні змінні це не дуже добре. Може замість EventListenerа просто вставити це в функцію? Чи те, що змінна буде глобальною, це неважливо?
+window.addEventListener('load', () => {
   const formData = new FormData(form);
   for (formItem of [...formData]) {
-    if (localStorage.getItem(formItem[0])) {
+    if (formObject[formItem[0]]) {
       form.querySelector(`[name='${formItem[0]}']`).value =
-        localStorage.getItem(formItem[0]);
+        formObject[formItem[0]];
     }
   }
 });
@@ -19,19 +25,21 @@ window.addEventListener('load', () => {
 form.addEventListener(
   'input',
   throttle(e => {
-    localStorage.setItem(e.target.name, e.target.value);
+    formObject[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formObject));
   }, 500)
 );
 
 // submit and clear storage
 form.addEventListener('submit', e => {
   e.preventDefault();
+  const resultingFormObject = {};
 
   const formData = new FormData(e.currentTarget);
   for (formItem of [...formData]) {
-    localStorage.removeItem(formItem[0]);
-    console.log(`${formItem[0]} value: ${formItem[1]}`);
+    resultingFormObject[formItem[0]] = formItem[1];
   }
-
+  console.log(resultingFormObject);
+  localStorage.removeItem('feedback-form-state');
   form.reset();
 });
