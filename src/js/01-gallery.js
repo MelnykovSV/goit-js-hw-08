19679e034a1c41703cc6fa23658f0dbd6c03bbdc
MelnykovSV <svmelnykov@gmail.com>
2023-02-07 11:35:25 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,8 +2,6 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Change code below this line
-
 const gallery = document.querySelector('.gallery');
 //  ==========================MARKUP CREACTION==============================
 const markup = galleryItems
@@ -16,7 +14,7 @@ gallery.insertAdjacentHTML('beforeend', markup);
 
 // //  ==========================EVENTS HANDLING==============================
 
-var $gallery = new SimpleLightbox('.gallery a', {
+const lightboxGallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
